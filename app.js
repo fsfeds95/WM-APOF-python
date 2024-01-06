866d9827249574247681f28b5a5ef17ff5c4a2d1
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-01-06 10:01:01 -0400

    Actualizar app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8225;
 
 // Ruta "/backdrop"
-app.get('/b', async (req, res) => {
+app.get('/backdrop', async (req, res) => {
   const { image } = req.query;
 
   // Verificar si se proporcionó un enlace de imagen
@@ -15,16 +15,16 @@ app.get('/b', async (req, res) => {
 
   try {
     // Escalar la imagen a 1280x720
-    const scaledimage = await sharp(image)
+    const scaledImage = await sharp(image)
       .resize(1280, 720)
       .toBuffer();
 
     // Agregar marca de agua
-    const watermarkedimage = await addWatermark(scaledimage, 'Wtxt-backdrop.png', 1280, 720);
+    const watermarkedImage = await addWatermark(scaledImage, 'wm-backdrop.png', 1280, 720);
 
     // Enviar la imagen de salida como JPEG con calidad del 90%
     res.contentType('image/jpeg');
-    res.send(watermarkedimage);
+    res.send(watermarkedImage);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error al procesar la imagen');
@@ -32,7 +32,7 @@ app.get('/b', async (req, res) => {
 });
 
 // Ruta "/poster"
-app.get('/p', async (req, res) => {
+app.get('/poster', async (req, res) => {
   const { image } = req.query;
 
   // Verificar si se proporcionó un enlace de imagen
@@ -42,16 +42,16 @@ app.get('/p', async (req, res) => {
 
   try {
     // Escalar la imagen a 720x1280
-    const scaledimage = await sharp(image)
+    const scaledImage = await sharp(image)
       .resize(720, 1280)
       .toBuffer();
 
     // Agregar marca de agua
-    const watermarkedimage = await addWatermark(scaledimage, 'Wtxt-poster.png', 720, 1280);
+    const watermarkedImage = await addWatermark(scaledImage, 'wm-poster.png', 720, 1280);
 
     // Enviar la imagen de salida como JPEG con calidad del 90%
     res.contentType('image/jpeg');
-    res.send(watermarkedimage);
+    res.send(watermarkedImage);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error al procesar la imagen');
